cc173c03186a5a2ef4906cb664040a17895d331a
leeviojala <leevi.ojala@gmail.com>
2021-03-15 08:52:33 +0200

    header content

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,7 +5,8 @@ export default function Head() {
   return (
     <Alert severity="info" style={{ paddingTop: "16px" }}>
       <AlertTitle>Rokotustilanne Suomessa</AlertTitle>
-      Alla on kuvattuna suomen koronarokotustilanne reaaliaikaisesti.
+      Alla on kuvattuna suomen koronarokotustilanne{" "}
+      <strong>ensimmäisen rokoteannoksen</strong> osalta reaaliaikaisesti.
       <strong> Tämä data ei välttämättä pidä paikkaansa!</strong>
     </Alert>
   );
